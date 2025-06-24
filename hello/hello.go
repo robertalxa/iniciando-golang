89a32df86d31257effb3eecf5e0655a4be38eafe
Robert Alexandre <robert.alexandrea7@gmail.com>
2025-06-24 01:45:52 -0300

    Iniciando estudos em GOLang

---
+++ hello/hello.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Olá mundo")
+	fmt.Println("Este é meu primeiro programa em GO")
+}
+
+// Para buildar o executável é só rodar go build *nome do arquivo.go*
+// Para não precisar ficar dando build toda hora é só rodar "go run arquivo.go"
