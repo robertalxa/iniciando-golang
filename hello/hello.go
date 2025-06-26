b722a5d46b993b1112a382ea19d7f2a093efcce2
Robert Alexandre <robert.alexandrea7@gmail.com>
2025-06-25 23:31:23 -0300

    Declarando variáveis com :=

--- hello/hello.go
+++ hello/hello.go
@@ -9,8 +9,10 @@ func main() {
 	var nome = "Douglas"
 	var versao float32 = 1.1 // Sempre declarar o tipo dos floats pois existem dois
 	var idade = 24
+	variavelDeclaradaSemVar := "Se declara com :="
 	fmt.Println("Olá, sr.", nome, "! Sua idade é", idade)
 	fmt.Println("Este programa está na versão", versao)
+	fmt.Println("Como declara variável de maneira curta?", variavelDeclaradaSemVar)
 
 	fmt.Println("O tipo da variável nome é ", reflect.TypeOf(nome))
 	fmt.Println("O tipo da variável idade é ", reflect.TypeOf(idade))
