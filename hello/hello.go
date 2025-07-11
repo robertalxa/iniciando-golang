f18d582835de2d08f5766521f88cd216dab206e1
Robert Alexandre <robert.alexandrea7@gmail.com>
2025-07-11 00:46:49 -0300

    Realizando requisições com módulo http e tratando funções com mais de um retorno

--- hello/hello.go
+++ hello/hello.go
@@ -2,17 +2,20 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 )
 
 func main() {
+	// _, idade := devolveNomeEIdade() Esse underline faz o go ignorar o primeiro retorno
+	// fmt.Println("tenho ", idade, " anos")
 	exibeIntroducao()
 	exibeMenu()
 	comando := leComando()
 
 	switch comando {
 	case 1:
-		fmt.Println("Monitorando")
+		iniciarMonitoramento()
 	case 2:
 		fmt.Println("Exibindo logs")
 	case 0:
@@ -39,6 +42,20 @@ func leComando() int {
 	var comandoLido int
 	fmt.Scan(&comandoLido) // Aqui eu não preciso passar o modificador da variável
 	return comandoLido
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando")
+	site := "https://www.alura.com.br"
+	response, _ := http.Get(site)
+	fmt.Println("Retorno da requisição: ", response)
+
+}
+
+func devolveNomeEIdade() (string, int) {
+	nome := "Robert"
+	idade := 24
+	return nome, idade
 }
 
 // Para buildar o executável é só rodar go build *nome do arquivo.go*
