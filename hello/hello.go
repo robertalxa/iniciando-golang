1e9e78e72c44597539f9fc55ae9cf95d5d270c45
Robert Alexandre <robert.alexandrea7@gmail.com>
2025-07-22 20:54:01 -0300

    Exibindo logs no terminal

--- hello/hello.go
+++ hello/hello.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"strconv"
@@ -28,7 +29,7 @@ func main() {
 		case 1:
 			iniciarMonitoramento()
 		case 2:
-			fmt.Println("Exibindo logs")
+			imprimeLogs()
 		case 0:
 			fmt.Println("Saindo do programa")
 			os.Exit(0)
@@ -126,6 +127,37 @@ func registraLog(site string, status bool) {
 	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + " - online: " + strconv.FormatBool(status) + "\n")
 
 	arquivo.Close()
+}
+
+func imprimeLogs() {
+	// Maneira com openfile
+	// const permissao = 0666
+	// arquivo, err := os.OpenFile("log.txt", os.O_RDONLY, permissao)
+
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+
+	// reader := bufio.NewReader(arquivo)
+
+	// for {
+	// 	linha, err := reader.ReadString('\n')
+	// 	linha = strings.TrimSpace(linha)
+	// 	if err == io.EOF {
+	// 		break
+	// 	}
+	// 	fmt.Println(linha)
+	// }
+
+	// Maneira com ioutil
+	arquivo, err := ioutil.ReadFile("log.txt")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(arquivo))
+
 }
 
 func devolveNomeEIdade() (string, int) {
