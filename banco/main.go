c0e50983029b3ef68ecaf013c93003e876fc9b2a
Robert Alexandre <robert.alexandrea7@gmail.com>
2025-08-18 22:36:53 -0300

    Comparando tipos

--- banco/main.go
+++ banco/main.go
@@ -16,14 +16,25 @@ func main() {
 		numeroConta:   2000,
 		saldo:         100,
 	}
-	conta2 := ContaCorrente{"Ariana Grande", 222, 333, 100}
-	fmt.Println(conta1)
-	fmt.Println(conta2)
 
-	var contaBieber *ContaCorrente
+	conta2 := ContaCorrente{
+		titular:       "Robert",
+		numeroAgencia: 0001,
+		numeroConta:   2000,
+		saldo:         100,
+	}
+	fmt.Println(conta1 == conta2) // O Go entende que tem que comparar o conteúdo e que o mesmo é de fato igual
 
-	contaBieber = new(ContaCorrente)
-	contaBieber.titular = "Bieber"
-	contaBieber.saldo = 5000000000
-	fmt.Println(*contaBieber)
+	// fmt.Println(conta1)
+	// fmt.Println(conta2)
+
+	// conta2 := ContaCorrente{"Ariana Grande", 222, 333, 100}
+	// fmt.Println(conta2)
+
+	// var contaBieber *ContaCorrente
+
+	// contaBieber = new(ContaCorrente)
+	// contaBieber.titular = "Bieber"
+	// contaBieber.saldo = 5000000000
+	// fmt.Println(*contaBieber)
 }
