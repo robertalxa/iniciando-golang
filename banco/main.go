1be77b40ccb6f076d8baedc75a8619358d3e0a57
Robert Alexandre <robert.alexandrea7@gmail.com>
2025-08-18 22:43:45 -0300

    Trabalhando cum uma função que utiliza uma conta genérica para saque

--- banco/main.go
+++ banco/main.go
@@ -9,32 +9,25 @@ type ContaCorrente struct {
 	saldo         float64
 }
 
+func (c *ContaCorrente) sacar(valorSaque float64) string {
+	podeSacar := valorSaque <= c.saldo
+	if podeSacar {
+		c.saldo -= valorSaque
+		return "Saque realizado com sucesso"
+	}
+
+	return "Saldo insuficiente!"
+}
+
 func main() {
-	conta1 := ContaCorrente{
+	conta := ContaCorrente{
 		titular:       "Robert",
 		numeroAgencia: 0001,
 		numeroConta:   2000,
 		saldo:         100,
 	}
 
-	conta2 := ContaCorrente{
-		titular:       "Robert",
-		numeroAgencia: 0001,
-		numeroConta:   2000,
-		saldo:         100,
-	}
-	fmt.Println(conta1 == conta2) // O Go entende que tem que comparar o conteúdo e que o mesmo é de fato igual
-
-	// fmt.Println(conta1)
-	// fmt.Println(conta2)
-
-	// conta2 := ContaCorrente{"Ariana Grande", 222, 333, 100}
-	// fmt.Println(conta2)
-
-	// var contaBieber *ContaCorrente
-
-	// contaBieber = new(ContaCorrente)
-	// contaBieber.titular = "Bieber"
-	// contaBieber.saldo = 5000000000
-	// fmt.Println(*contaBieber)
+	fmt.Println(conta)
+	fmt.Println(conta.sacar(900))
+	fmt.Println(conta)
 }
